263f15bc4f86289e15c3180eebf855a0419a5d10
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2019-12-18 18:17:17 -0600

    Upload role dành riêng cho bot tạo phòng

---
+++ commands/moderation/pdrole.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "understand",
+    category: "moderation",
+    description: "Hiểu và nắm bắt rule khi vào.",
+    usage: "_understand",
+    run: async (client, message, args) => {
+        var member = message.author.id
+        var role = message.guild.roles.find(role => role.name === "phòng đôi");
+        message.member.addRole(role);
+        return message.reply("Add role thành công!");
+    }
+}
