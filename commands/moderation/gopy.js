00d7512ed32412a208820944914ab7e9e5999ec2
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2019-12-19 10:47:19 -0600

    Fix bug

--- commands/moderation/gopy.js
+++ commands/moderation/gopy.js
@@ -7,8 +7,8 @@ module.exports = {
         const channel = message.guild.channels.find(c => c.name === "gop-y")
         if (!channel)
             return message.channel.send("Couldn't find a `#gop-y` channel").then(m => m.delete(5000));
-        if (args.length < 0)
-            return message.reply("Nothing to say?").then(m => m.delete(5000));
+        if (!args[0])
+            return message.reply("Góp ý đéo ghi chữ nào tao tán chết mẹ mày giờ").then(m => m.delete(5000));
         let date_ob = new Date();
         let date = ("0" + date_ob.getDate()).slice(-2);
         let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
