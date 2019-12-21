1d893014cf13ee2d54e0c5d58ccb5cfaa8ccc7d3
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2019-12-21 12:20:21 -0600

    Update avatar.js

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -1,8 +1,10 @@
 module.exports = {
-    name: "avatar",
+    name: "test",
     description: "Get avatar ",
     usage: "_avatar <tag>",
     run: (client, message, args) => {
+        if (!args[0])
+            return message.channel.send(message.author.avatarURL)
         const member = message.mentions.members.first() || message.guild.members.get(args[0]);
         message.channel.send(member.user.displayAvatarURL)
 
