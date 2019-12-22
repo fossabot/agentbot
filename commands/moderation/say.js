e552f24b6db077b2368db27b1cbf609d9f050854
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2019-12-22 00:16:55 -0600

    Rút gọn say.js

--- commands/moderation/say.js
+++ commands/moderation/say.js
@@ -1,7 +1,5 @@
-const { RichEmbed } = require("discord.js");
-
 module.exports = {
-    name: "say",
+    name: "test",
     description: "Says your input via the bot",
     usage: "<input>",
     run: (client, message, args) => {
@@ -13,16 +11,9 @@ module.exports = {
         if (args.length < 0)
             return message.reply("Nothing to say?").then(m => m.delete(5000));
 
-        const roleColor = message.guild.me.highestRole.hexColor;
 
-        if (args[0].toLowerCase() === "embed") {
-            const embed = new RichEmbed()
-                .setDescription(args.slice(1).join(" "))
-                .setColor(roleColor === "#000000" ? "#ffffff" : roleColor);
-
-            message.channel.send(embed);
-        } else {
-            message.channel.send(args.join(" "));
+        message.channel.send(args.join(" "));
         }
     }
-}
+
+    
