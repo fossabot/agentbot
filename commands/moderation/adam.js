bed3c8913377568dbcec7e6a7ebd24631344eb81
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2019-12-25 20:02:59 -0600

    Update adam.js

--- commands/moderation/adam.js
+++ commands/moderation/adam.js
@@ -9,8 +9,8 @@ module.exports = {
             return message.reply("Đéo tag tao tán chết mẹ")
                 .then(m => m.delete(5000));
         }
-        if (message.author.id != '199276237250625536' && message.author.id != '455935236262592512') {
-            return message.reply("Code này dành riêng cho Saddu và Duy")
+        if (message.author.id != '199276237250625536' && message.author.id != '455935236262592512' && message.author.id != '168430324152270849') {
+            return message.reply("Code này dành riêng cho Saddu, Duy và Issac")
         }
         let user = message.mentions.members.first() || message.guild.members.get(args[0]);
         var role = message.guild.roles.find(role => role.name === "adam");
