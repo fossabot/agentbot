9835cc8ce1ddf74e3adafadcbbfa2bc3cf722ac3
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2019-12-25 19:27:24 -0600

    Update report.js

--- commands/moderation/report.js
+++ commands/moderation/report.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "report",
     category: "moderation",
     description: "Reports a member",
-    usage: "<mention, id>",
+    usage: "_report <tag> <lý do>",
     run: async (client, message, args) => {
         if (message.deletable) message.delete();
 
