54e583397105e0dccd8ddd1c460315bd14476bd4
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2019-12-28 03:55:15 -0600

    Upload weather.js

---
+++ commands/info/weather.js
@@ -0,0 +1,28 @@
+var getJSON = require('get-json')
+const { RichEmbed } = require("discord.js");
+module.exports = {
+    name: "weather",
+    description:"Get JSON",
+    usage:"_weather <zipcode>,<country code (2 letters)>",
+    run: (client, message, args) => {
+    var city = args[0];
+    let url = `http://api.openweathermap.org/data/2.5/weather?zip=${city}&units=metric&appid=e79a43d4648af5dc9281c39cf638e375&lang=vi`;
+    getJSON(url, function(error, response){
+        const embed = new RichEmbed()
+        .setTitle("Link JSON")
+        .setURL(url)
+        .addField("Tình trạng lỗi:", error, true)
+        .addField("Đây là thành phố:",response.name,true)
+        .addField("Mã quốc gia:",response.sys.country)
+        .addField("Thời tiết:",response.weather[0].main,true)
+        .addField("Thời tiết chi tiết:",response.weather[0].description,true)
+        .addField("Nhiệt độ trung bình:",response.main.temp,true)
+        .addField("Nhiệt độ thấp nhất trong ngày:",response.main.temp_min,true)
+        .addField("Nhiệt độ cao nhất trong ngày:",response.main.temp_max,true)
+        .setFooter("Nhiệt độ tính bằng độ C")
+
+        message.channel.send(embed)
+
+    });
+}
+}
