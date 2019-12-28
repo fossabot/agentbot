bedbb603f80ff4fc4c163a1c076eca8cdd342ecc
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2019-12-27 21:09:41 -0600

    Upload coloradd.js and tempmute.js

---
+++ commands/moderation/coloradd.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "coloradd",
+    description:"Add role with color",
+    usage:"_coloradd <hexcolor or r,g,b> <name>",
+    run: (client, message, args) => {
+    if (!message.member.hasPermission("MANAGE_ROLES"))
+      return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000));
+    if (!args[0])
+      return message.reply("Không chọn màu thì kêu tao làm gì")
+    if (!args[1])
+      return message.reply("Không đặt tên role tao tạo bằng niềm tin").then(m => m.delete(5000))
+    let name = args.slice(1).join(" ")
+        message.guild.createRole({
+            name: name,
+            color: args[0],
+            position: '92',
+          })
+        return message.reply("Done!")
+    }
+    }
---
+++ commands/moderation/tempmute.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: "tempmute",
+    description: "Says your input via the bot",
+    usage: "_tempmute <@tag> <time> <reason>",
+    run: (client, message, args) => {
+    if (!message.member.hasPermission("MANAGE_ROLES"))
+      return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000));
+    // This is the role you want to assign to the user
+    let mutedRole = message.guild.roles.find(role => role.name == "Muted");
+    // This is the member you want to mute
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    let minutes = args[1];
+    let reason = args.slice(2).join(" ");
+    // Mute the user
+    member.addRole(mutedRole)
+    message.channel.send(`Muted by ${message.author.tag} for ${minutes} minutes. Reason: ${reason}`);
+  
+    // Unmute them after x minutes
+    setTimeout(() => {
+      message.guild.member(member).removeRole(mutedRole);
+    }, minutes * 60000);
+    }
+}
