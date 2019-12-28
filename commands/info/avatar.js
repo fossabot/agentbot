0a38f94d21d9f6fddfe292b42827b9ce79904c2f
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2019-12-27 21:08:13 -0600

    Upgrade avatar.js

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -1,12 +1,18 @@
+const { RichEmbed } = require("discord.js");
 module.exports = {
     name: "avatar",
     description: "Get avatar ",
     usage: "_avatar <tag>",
     run: (client, message, args) => {
-        if (!args[0])
-            return message.channel.send(message.author.avatarURL)
-        const member = message.mentions.members.first() || message.guild.members.get(args[0]);
-        message.channel.send(member.user.displayAvatarURL)
-
+    var member = message.mentions.members.first() || message.guild.members.get(args[0]);
+        if (!args[0]){
+            let embed1 = new RichEmbed()
+                .setImage(message.author.avatarURL)
+            message.channel.send(embed1)
+        } else {
+            const embed = new RichEmbed()
+                .setImage(member.user.displayAvatarURL)
+            message.channel.send(embed)
+        }
     }
 }
