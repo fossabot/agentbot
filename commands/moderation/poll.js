d3df7463c994b0de732b86470c65afb2ac69092d
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-01-01 14:19:07 -0600

    Poll system

---
+++ commands/moderation/poll.js
@@ -0,0 +1,28 @@
+const { RichEmbed } = require("discord.js");
+module.exports = {
+    name: "poll",
+    category: "moderation",
+    description: "Tạo poll",
+    usage: "_poll <what to poll>",
+    run: async (client, message, args) => {
+        if(!args[0]) return message.channel.send("Đéo ghi gì tao tạo poll bằng chim =))")
+        if(message.member.roles.some(r=>["admin","mod"].includes(r.name)) || message.author.id == "455935236262592512"){
+            const channel = message.guild.channels.find(c => c.name === "poll")
+            if(!channel) return message.reply("Đéo tìm thấy phòng tên Poll")
+            const embed = new RichEmbed()
+                .setColor('RANDOM')
+                .setFooter('React to vote!')
+                .setDescription(args.join(' '))
+                .setTitle(`Poll created by ${message.author.username}`);
+            let msg = await channel.send(embed);
+            await msg.react('✅');
+            await msg.react('❌');
+    
+            message.delete({timeout: 1000});
+        }else{
+            message.delete({timeout:1000})
+            return message.reply("Code này dành cho admin và mod").then(m=>m.delete(1000))
+        }
+
+    }
+}
