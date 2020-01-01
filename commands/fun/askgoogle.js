12a15e181b3a8b2f0a1f69b276925ff3a7567630
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2019-12-31 19:34:39 -0600

    upload askgoogle.js

---
+++ commands/fun/askgoogle.js
@@ -0,0 +1,14 @@
+const encode = require('strict-uri-encode')
+module.exports = {
+    name: "askgoogle",
+    run: async (client, message, args, tools) => {
+        message.delete()
+        if (!args[0]){
+            return message.reply(`**Mày đéo nhập gì tao tìm bằng cu**`)
+        } 
+        
+        let question = encode(args.join(' '));
+        let link = `https://lmgtfy.com/?q=${question}`;
+        message.channel.send(`**<${link}>**`)
+    }
+}
