962c14846e9c40f69bd0a1bee5980f0ace5e0ead
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-01-08 21:27:00 -0600

    Update translate.js

--- commands/fun/translate.js
+++ commands/fun/translate.js
@@ -1,5 +1,6 @@
 var getJSON = require("get-json")
 const { RichEmbed } = require("discord.js")
+const encode = require('strict-uri-encode')
 module.exports = {
     name: "translate",
     category: "fun",
@@ -8,14 +9,14 @@ module.exports = {
     run: (client,message,args) => {
         var key = `trnsl.1.1.20200109T024819Z.ed62bc942ddc7e0c.196d24a1ec66daa8f2fc9a3c3618c522dddc2931`
         let lang = args[0]
-        let word = args.slice(1).join(' ')
+        let word = encode(args.slice(1).join(' '));
         let url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${word}&lang=${lang}`
         getJSON(url, function(error,response){
             if (response.code == "200"){
                 const embed = new RichEmbed()
                     .setDescription(`Hệ thống dịch của AgentBot: `)
                     .addField("Dịch từ: ", `${response.lang}`)
-                    .addField("Từ trước khi dịch: ", `${word}`)
+                    .addField("Từ trước khi dịch: ", `${args.slice(1).join(' ')}`)
                     .addField("Từ sau khi dịch sang",`${response.text[0]}`)
                 return message.channel.send(embed)
             }else{
