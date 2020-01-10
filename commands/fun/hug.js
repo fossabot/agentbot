4716653591b7c288e94c52042685b77d0fc13d64
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-01-10 12:03:23 -0600

    Fix a lot of fricking bugs

--- commands/fun/hug.js
+++ commands/fun/hug.js
@@ -8,19 +8,25 @@ module.exports = {
     run: (client, message, args) => {
         let url = `https://some-random-api.ml/animu/hug`
         getJSON(url, function(error,response){
-        if (!args[0]){
-            const embed1 = new RichEmbed()
-                .setDescription(`${message.member.displayName} đã ôm tất cả mọi người <3`)
-                .setImage(response.link)
-                .setFooter(`By AgentBot đẹp trai`)
-            return message.channel.send(embed1)
-        }
-        let person = message.mentions.members.first() || message.guild.members.get(args[0]);
-            const embed = new RichEmbed()
-            .setDescription(`Awwww, <@${message.member.id}> đã ôm <@${person.id}> <3`)
-            .setImage(response.link)
-            .setFooter(`By AgentBot đẹp trai`)
-        return message.channel.send(embed)
-        });
+            if(!error){
+                if (!args[0]){
+                    const embed1 = new RichEmbed()
+                        .setDescription(`${message.member.displayName} đã ôm tất cả mọi người <3`)
+                        .setImage(response.link)
+                        .setFooter(`By AgentBot đẹp trai`)
+                    return message.channel.send(embed1)
+                }
+                let person = message.mentions.members.first() || message.guild.members.get(args[0]);
+                    const embed = new RichEmbed()
+                    .setDescription(`Awwww, <@${message.member.id}> đã ôm <@${person.id}> <3`)
+                    .setImage(response.link)
+                    .setFooter(`By AgentBot đẹp trai`)
+                return message.channel.send(embed)
+            } else {
+                return message.channel.send("Bot lỗi khi cố gắng lấy hình, hãy thử lại sau")
+            }
+            });
     }
-}
+            
+    }
+
