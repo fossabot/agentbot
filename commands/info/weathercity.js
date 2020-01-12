31d40a53475a99c6a859e2bd072b8a7481f2322d
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-01-11 22:14:44 -0600

    Update weathercity.js

--- commands/info/weathercity.js
+++ commands/info/weathercity.js
@@ -7,7 +7,7 @@ module.exports = {
     usage:"_weathercity <cityname>,<country code (2 letters)>",
     run: (client, message, args) => {
     var city = args[0];
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e79a43d4648af5dc9281c39cf638e375&lang=vi`;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=URAPPID&lang=vi`;
     getJSON(url, function(error, response){
         if(error)
             return message.reply("Ghi đúng lệnh chưa địt mẹ mày =)). Ghi tên thành phố éo phải ghi zip code")
