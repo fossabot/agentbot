dfda1406cd6798503ffd572e2464da1bf375b98e
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-01-11 22:14:59 -0600

    Update weatherzip.js

--- commands/info/weatherzip.js
+++ commands/info/weatherzip.js
@@ -7,7 +7,7 @@ module.exports = {
     usage:"_weatherzip <zipcode>,<country code (2 letters)>",
     run: (client, message, args) => {
     var city = args[0];
-    let url = `http://api.openweathermap.org/data/2.5/weather?zip=${city}&units=metric&appid=e79a43d4648af5dc9281c39cf638e375&lang=vi`;
+    let url = `http://api.openweathermap.org/data/2.5/weather?zip=${city}&units=metric&appid=URAPPID&lang=vi`;
     getJSON(url, function(error, response){
         if(error)
             return message.reply("Ghi đúng lệnh chưa địt mẹ mày =)). Ghi zipcode éo phải ghi tên thành phố")
