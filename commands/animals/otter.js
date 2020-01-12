450fc931c2c2b43488ee2bcc755a34e506d75add
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-01-11 22:17:06 -0600

    Update otter.js

--- commands/animals/otter.js
+++ commands/animals/otter.js
@@ -6,7 +6,7 @@ module.exports = {
     description: "Gởi hình rái cá :) ",
     usage:"_otter",
     run: (client, message, args) => {
-        let url = `https://api.giphy.com/v1/gifs/random?api_key=jw9hRDeB0uuU5PPqIHjKk6UZgrMnJh3q&tag=otter&rating=PG-13`
+        let url = `https://api.giphy.com/v1/gifs/random?api_key=URAPIKEY&tag=otter&rating=PG-13`
         getJSON(url, function(error,response){
         let person = message.mentions.members.first() || message.guild.members.get(args[0]);
             const embed = new RichEmbed()
