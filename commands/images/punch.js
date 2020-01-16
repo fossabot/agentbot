21c471a2f07e37bd37241d0a57ad3d1ed666f43c
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-01-16 11:40:24 -0600

    Upload punch.js

---
+++ commands/images/punch.js
@@ -0,0 +1,28 @@
+const { RichEmbed } = require("discord.js");
+var getJSON = require("get-json");
+module.exports = {
+    name: "punch",
+    category: "images",
+    description: "Punch someone :D ",
+    usage:"_punch <@tag>",
+    run: (client, message, args) => {
+        let url = `https://api.giphy.com/v1/gifs/random?api_key=jw9hRDeB0uuU5PPqIHjKk6UZgrMnJh3q&tag=punch&rating=R`
+        getJSON(url, function(error,response){
+        let person = message.mentions.members.first() || message.guild.members.get(args[0]);
+        if (!args[0] || (!person)){
+            const embed1 = new RichEmbed()
+                .setDescription(`<@${message.member.id}> đã tự đấm chính mình 👊`)
+                .setImage(response.data.images.original.url)
+                .setFooter(`AgentBot đoán là thằng này bị ngu =))))`)
+            return message.channel.send(embed1)
+        } else {
+
+            const embed = new RichEmbed()
+                .setDescription(`<@${message.member.id}> đã đấm vỡ mồm 🤜 <@${person.id}> 🤛`)
+                .setImage(response.data.images.original.url)
+                .setFooter(`By AgentBot đẹp trai`)
+            return message.channel.send(embed)
+        }
+        });
+    }
+}
