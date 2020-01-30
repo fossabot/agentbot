440d08fa161c359b0ca8c8755c8aba013b0b042a
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-01-30 01:01:58 -0600

    Fix Bugs

--- commands/fun/calc.js
+++ commands/fun/calc.js
@@ -3,16 +3,17 @@ var {wolfarm_key} = require('../../apikey.json')
 var getJSON = require('get-json')
 const { RichEmbed } = require('discord.js')
 module.exports = {
-    name: "calc",
+    name: "tcalc",
     category: "fun",
     description: "Tính toán và sẽ đưa ra các bước giải",
     usage:"_calc <query>",
     run: (client, message, args) => {
         var query = encode(args.join(' '))
-        var url = `http://api.wolframalpha.com/v2/query?input=${query}&podstate=Result__Step-by-step+solution&format=plaintext&output=JSON&appid=${wolfarm_key}`
+        var url = `http://api.wolframalpha.com/v2/query?input=solve%20${query}&podstate=Result__Step-by-step+solution&format=plaintext&output=JSON&appid=${wolfarm_key}`
         getJSON(url, function(error, response){
             if (error) return message.channel.send(`Bot lỗi khi đang làm việc, vui lòng thử lại sau.`)
             if (response.queryresult.success == false) return message.reply(`Input ncc, tao tìm không ra câu trả lời.`)
+            console.log(response)
             var output = response.queryresult.pods[1].subpods[1]
             const embed = new RichEmbed()
                 .setAuthor(`Agent Bot`)
