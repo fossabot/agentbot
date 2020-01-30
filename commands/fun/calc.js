8482e85da08fa9bd9978b067066970d0d8cd1d6c
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-01-30 00:45:14 -0600

    Upgraded

--- commands/fun/calc.js
+++ commands/fun/calc.js
@@ -1,23 +1,25 @@
-const math = require('mathjs');
-const { RichEmbed } = require("discord.js");
+const encode = require('strict-uri-encode')
+var {wolfarm_key} = require('../../apikey.json')
+var getJSON = require('get-json')
+const { RichEmbed } = require('discord.js')
 module.exports = {
     name: "calc",
     category: "fun",
-    description:"tính toán nhanh",
-    run: async (client, message, args, tools) => {
-        if (!args[0]) return message.reply("Không nhập số tao tính bằng cu.")
-        let resp;
-        try {
-            resp = math.evaluate(args.join(' '));
-        }catch (e) {
-            return message.channel.send("Tao không giải được, một là mày ngu hai là tao ngu!")
-        }
-        const embed = new RichEmbed()
-            .setColor(0xffffff)
-            .setTitle('Math Calculation')
-            .addField('Input', `\`\`\`${args.join(' ')}\`\`\``)
-            .addField('Output', `\`\`\`js\n${resp}\`\`\``)
-        message.channel.send(embed)
+    description: "Tính toán và sẽ đưa ra các bước giải",
+    usage:"_calc <query>",
+    run: (client, message, args) => {
+        var query = encode(args.join(' '))
+        var url = `http://api.wolframalpha.com/v2/query?input=${query}&podstate=Result__Step-by-step+solution&format=plaintext&output=JSON&appid=${wolfarm_key}`
+        getJSON(url, function(error, response){
+            if (error) return message.channel.send(`Bot lỗi khi đang làm việc, vui lòng thử lại sau.`)
+            if (response.queryresult.success == false) return message.reply(`Input ncc, tao tìm không ra câu trả lời.`)
+            var output = response.queryresult.pods[1].subpods[1]
+            const embed = new RichEmbed()
+                .setAuthor(`Agent Bot`)
+                .setTitle(`Question: ${args.join(' ')}. ${output.title}`)
+                .setDescription(output.plaintext)
+                .setFooter(`Bot made by phamleduy04#9999`)
+            message.channel.send(embed)
+        });
     }
-
 }
