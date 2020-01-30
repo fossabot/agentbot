aa53d819c50b7637668be5ac0cf6944773e700d1
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-01-30 09:46:57 -0600

    Fix bugs

--- commands/info/whois.js
+++ commands/info/whois.js
@@ -7,16 +7,23 @@ module.exports = {
     aliases: ["who", "user", "info"],
     category: "info",
     description: "Returns user information",
-    usage: "[username | id | mention]",
+    usage: "_whois <tag,username,ID>",
     run: (client, message, args) => {
         const member = getMember(message, args.join(" "));
-
+        let gtrole = message.guild.roles.get('663977844371619873')
+        let qgrole = message.guild.roles.get('663984586757505034')
+        let gamerole = message.guild.roles.get('663990034868994048')
+        let statusrole = message.guild.roles.get('668691327701090314')
         // Member variables
         const joined = formatDate(member.joinedAt);
         const roles = member.roles
-            .filter(r => r.id !== message.guild.id)
-            .map(r => r).join(", ") || 'none';
-
+            .filter(r => r.id !== message.guild.id )
+            .map(r => r)
+        roles.splice(roles.indexOf(gtrole),1);
+        roles.splice(roles.indexOf(qgrole),1);
+        roles.splice(roles.indexOf(gamerole),1);
+        roles.splice(roles.indexOf(statusrole),1);
+        roles.join(", ") || 'none';
         // User variables
         const created = formatDate(member.user.createdAt);
 
