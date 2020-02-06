cced15551b5649b3067474b02a7cbe8c7ed69957
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-02-06 00:02:16 -0600

    Fix bot lỗi khi sử dụng blacklist

--- commands/fun/pray.js
+++ commands/fun/pray.js
@@ -1,5 +1,4 @@
 const { getMember } = require("../../functions.js");
-const fs = require('fs')
 module.exports = {
     name: "pray",
     category: "fun",
@@ -10,13 +9,10 @@ module.exports = {
             return message.reply("Pray mà éo có tag là tao tán vỡ mồm đó 😡")
         }
         let person = getMember(message, args[0]);
-        if (message.author.id=== person.id)
+        if (message.author.id=== person.id){
             return message.reply("Có thờ có thiêng có duyên chết liền. Cầu cho người khác chứ cầu cho mình hoài vậy.");
-        var blacklist = fs.readFileSync("./blacklist.txt","utf8").split('\n')
-        if (blacklist.indexOf(message.author.id) > -1) {
-            message.channel.send(`🙏 ${message.member.displayName} prays for ${person.displayName} \n Wish you the best of luck!`);
         } else {
-            message.reply(`Bạn đang nằm trong blacklist, bạn không thể sử dụng lệnh này!`)
+            message.channel.send(`🙏 ${message.member.displayName} prays for ${person.displayName} \n Wish you the best of luck!`);
         }
 
 
