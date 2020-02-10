c658a3dd8afc2f983b3c871d92005d853cfe5693
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-02-09 19:44:41 -0600

    Upload corona.js

---
+++ commands/info/corona.js
@@ -0,0 +1,31 @@
+var request = require('request')
+const {RichEmbed} = require('discord.js')
+const url = 'https://en.wikipedia.org/api/rest_v1/page/html/2019%E2%80%9320_Wuhan_coronavirus_outbreak_by_country_and_territory?stash=true'
+module.exports = {
+    name: "corona",
+    category: "info",
+    description: "Thông tin về coronavirus",
+    usage: "_corona",
+    run: async (client, message, args) => {
+        request(url, function (error, response, request){
+            if (error) return message.channel.send(`Bot lỗi, status code: ${response && response.statusCode}`)
+            var begin = request.indexOf('<th align="right">')
+            var end = request.indexOf('<th></th></tr>');
+            var solieu = request.slice(begin,end)
+            var solieu = solieu.replace(/[^a-zA-Z0-9 ]/g, "");
+            var solieu = solieu.replace(/t|h|a|l|i|g|r|n/g, "");
+            var solieu = solieu.split(" ");
+            var xacnhan = solieu.slice(1,2);
+            var die = solieu.slice(2,3);
+            var recoveries= solieu.slice(3,4);
+            const embed = new RichEmbed()
+                .setAuthor(`Lưu ý: Thông tin cập nhật về bot không phải thời gian thực!`)
+                .setTitle(`Thông tin về virus Corona aka nCoV`)
+                .addField(`Số lượng ca nhiễm: `,`${xacnhan} ca`)
+                .addField(`Số người chết: `,`${die} người`)
+                .addField(`Số người bình phục: `,`${recoveries} người`)
+                .setFooter(`Nguồn: Wikipedia. Made by phamleduy04#9999`)
+            message.channel.send(embed)                
+        })
+    }
+}
