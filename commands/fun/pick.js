8719ffc8ee5f4d8cc7efce4279d26afaa1ed0aa0
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-02-11 22:25:48 -0600

    Upload pick.js

---
+++ commands/fun/pick.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "pick",
+    category: "fun",
+    description: "Random pick",
+    usage: "_pick <stuff,stuff2,.....>",
+    run: async (client, message, args) => {
+        var pick_wordlist = args.join(' ').split(',')
+        const random = pick_wordlist[Math.floor(Math.random() * pick_wordlist.length)];
+        return message.channel.send(random)
+    }
+}
