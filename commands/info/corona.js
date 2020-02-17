5301bb8f837c2c5878f1a813f7cf12f0bcbb06f9
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-02-17 03:04:05 +0000

    Đổi API

--- commands/info/corona.js
+++ commands/info/corona.js
@@ -1,6 +1,6 @@
 var request = require('request')
 const {RichEmbed} = require('discord.js')
-const url = 'https://en.wikipedia.org/api/rest_v1/page/html/2019%E2%80%9320_Wuhan_coronavirus_outbreak_by_country_and_territory?stash=true'
+const url = 'https://quohat.pythonanywhere.com'
 module.exports = {
     name: "corona",
     category: "info",
@@ -9,16 +9,10 @@ module.exports = {
     run: async (client, message, args) => {
         request(url, function (error, response, request){
             if (error) return message.channel.send(`Bot lỗi, status code: ${response && response.statusCode}`)
-            var begin = request.indexOf('<tr class="sortbottom">')
-            var end = request.indexOf('<tr style="text-align:center;" class="sortbottom">');
-            var solieu = request.slice(begin,end)
-            var solieu = solieu.replace(/[^a-zA-Z0-9 ]/g, "");
-            var solieu = solieu.replace(/c|s|o|t|r|l|a|h|m|b|p|e|i/g, "");
-            var solieu = solieu.trim().split(" ");
-            console.log(solieu)
-            var xacnhan = solieu.slice(2,3);
-            var die = solieu.slice(3,4);
-            var recoveries= solieu.slice(4,5);
+            var ketqua = request.split(' ')
+            var xacnhan = ketqua[0]
+            var die = ketqua[1]
+            var recoveries= ketqua[2]
             const embed = new RichEmbed()
                 .setAuthor(`Lưu ý: Thông tin cập nhật về bot không phải thời gian thực!`)
                 .setTitle(`Thông tin về virus Corona aka nCoV`)
@@ -26,7 +20,7 @@ module.exports = {
                 .addField(`Số người chết: `,`${die} người`)
                 .addField(`Số người bình phục: `,`${recoveries} người`)
                 .setFooter(`Nguồn: Wikipedia. Made by phamleduy04#9999`)
-            message.channel.send(embed)                
+            message.channel.send(embed)            
         })
     }
 }
