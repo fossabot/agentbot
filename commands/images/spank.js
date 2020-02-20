1a4f3fbc1562de017905db59a06b4b279764626f
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-02-20 16:12:24 -0600

    Add more command

---
+++ commands/images/spank.js
@@ -0,0 +1,46 @@
+const request = require('request');
+const url = 'https://dankmemer.services/api/spank'
+const fs = require('fs')
+const {auth_dankmemer} = require('../../config.json')
+module.exports = {
+    name: "spank",
+    aliases: ['vodit'],
+    category: 'images',
+    description: 'Vỗ ass =))',
+    usage: '_spank <@tag>',
+    run: async (client, message, args) => {
+        var avatar1 = message.author.displayAvatarURL
+        var tag = message.mentions.members.first() || message.guild.members.get(args[0])
+        if (tag) {
+            var avaurl = tag.user.displayAvatarURL
+        } else {
+            var avaurl = message.author.displayAvatarURL
+        }
+        if (tag.user.id == '455935236262592512') var avaurl = avatar1
+        let file = fs.createWriteStream(`./pics.jpg`)
+        await new Promise((resolve, reject) => {
+            request({
+                uri: url,
+                headers: {
+                'Authorization': auth_dankmemer
+                },
+                qs: {
+                'avatar1': avatar1,
+                'avatar2': avaurl
+                },
+                gzip: false
+            })
+            .pipe(file)
+            .on('finish', () => {
+                resolve();
+            })
+            .on('error', (err) => {
+                reject(err);
+            })
+            })
+                .catch(error => {
+                    console.log(error)
+                })
+        message.channel.send({file:'./pics.jpg'})
+    }
+}
