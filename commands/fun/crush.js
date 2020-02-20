3a95cd3d0b70f79053a397e1337d5efdc0f9b5c1
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-02-19 20:19:11 -0600

    Set timeout for crush command

--- commands/fun/crush.js
+++ commands/fun/crush.js
@@ -1,26 +1,36 @@
+const talkedRecently = new Set();
+const ms = require('ms')
 module.exports = {
     name: "crush",
     category: "fun",
     description: "Tìm crush của bạn",
     run: async (client, message, args) => {
-        if (message.member.roles.has('663976426609049601')) { //Nam
-            person = message.guild.members
-                .filter(m => m.id !== message.author.id && !m.user.bot && !m.roles.has('663976426609049601'))
-                .random();
-            message.channel.send(`${person.displayName} muốn xơi ${message.member.displayName} từ lâu......`);       
-        } else if (message.member.roles.has('663976552392032278')) { //Nữ
-            person = message.guild.members
-                .filter(m => m.id !== message.author.id && !m.user.bot && !m.roles.has('663976552392032278'))
-                .random();
-            message.channel.send(`${person.displayName} muốn xơi ${message.member.displayName} từ lâu......`);  
-        } else if (message.member.roles.has('663999161913442304')) { //role LGBT
-            person = message.guild.members
-                .filter(m => m.id !== message.author.id && !m.user.bot)
-                .random();
-                message.channel.send(`${person.displayName} muốn xơi ${message.member.displayName} từ lâu......`);  
+        if (talkedRecently.has(message.author.id)) {
+            message.reply(`Vui lòng chờ sau 5 giây từ khi nhập lệnh......`)
         } else {
-            pick_role = client.channels.get("663965962503979020")
-            message.channel.send(`Bạn chưa pick role giới tính, vui lòng pick role tại ${pick_role}`)
-        }
+            talkedRecently.add(message.author.id)
+            if (message.member.roles.has('663976426609049601')) { //Nam
+                person = message.guild.members
+                    .filter(m => m.id !== message.author.id && !m.user.bot && !m.roles.has('663976426609049601'))
+                    .random();
+                message.channel.send(`${person.displayName} muốn xơi ${message.member.displayName} từ lâu......`);       
+            } else if (message.member.roles.has('663976552392032278')) { //Nữ
+                person = message.guild.members
+                    .filter(m => m.id !== message.author.id && !m.user.bot && !m.roles.has('663976552392032278'))
+                    .random();
+                message.channel.send(`${person.displayName} muốn xơi ${message.member.displayName} từ lâu......`);  
+            } else if (message.member.roles.has('663999161913442304')) { //role LGBT
+                person = message.guild.members
+                    .filter(m => m.id !== message.author.id && !m.user.bot)
+                    .random();
+                    message.channel.send(`${person.displayName} muốn xơi ${message.member.displayName} từ lâu......`);  
+            } else {
+                pick_role = client.channels.get("663965962503979020")
+                message.channel.send(`Bạn chưa pick role giới tính, vui lòng pick role tại ${pick_role}`)
+            }
+            setTimeout(() => {
+                talkedRecently.delete(message.author.id)
+            },ms('5s'))
+    }
 }
 }
