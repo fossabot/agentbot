4835acd2171db5759a199cc621c0a20a9734c992
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-02-28 20:30:04 -0600

    Fix lỗi k get dc id làm crash bot

--- commands/moderation/checkblacklist.js
+++ commands/moderation/checkblacklist.js
@@ -1,17 +1,24 @@
 const fs = require('fs')
-const {RichEmbed} = require('discord.js')
+const { RichEmbed } = require('discord.js')
 module.exports = {
     name: "checkblacklist",
-    aliases: ["cbl","blacklistcheck"],
+    aliases: ["cbl", "blacklistcheck"],
     category: "moderation",
     description: "Show all blacklist members",
     usage: "_checkblacklist",
-    run: async (client, message, args) => {
-        var get_list = fs.readFileSync("./blacklist.txt",'utf8').split('\n')
+    run: async(client, message, args) => {
+        var get_list = fs.readFileSync("./blacklist.txt", 'utf8').split('\n')
+        var usernames = []
+        get_list.slice(get_list.indexOf(' '))
+        get_list.forEach(id => {
+            var get_id = message.guild.members.get(id)
+            if (!get_id) return usernames.push(id)
+            usernames.push(get_id.user.tag)
+        })
         const embed = new RichEmbed()
             .setTitle(`List những người bị blacklist`)
-            .setDescription(get_list)
-            .setFooter(`Sử dụng lệnh _whois <ID> để tìm kiếm.`)
+            .setDescription(usernames)
+            .setFooter(`By phamleduy04#9999`)
         message.channel.send(embed)
     }
 }
