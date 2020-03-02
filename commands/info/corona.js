c1882166ca3d0e04b8d6920ff2ee3f25e6a8c667
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-03-01 20:47:34 -0600

    Update corona.js (xem chi tiết)
    Add thêm quốc gia: Australia, China, Sweden, Hong kong, France
    Clean code
    Đổi command Vietnam_Full thành ascii-table

--- commands/info/corona.js
+++ commands/info/corona.js
@@ -1,7 +1,7 @@
 const { RichEmbed } = require("discord.js");
 const url = "https://corona-api.kompa.ai/graphql";
 const graphql = require("graphql-request");
-const korean_list = ["korean", "kr", "hanquoc", "han"];
+const ascii = require('ascii-table');
 const query = `query countries {
     countries {
         Country_Region
@@ -34,12 +34,34 @@ const graphqlclient = new graphql.GraphQLClient(url, {
         AcceptLanguage: "vn-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5"
     },
 })
+const search = {
+    "vn": "Vietnam",
+    "ca": "Canada",
+    "us": "US",
+    "kr": "South Korea",
+    "au": "Austria",
+    "cn": "Mainland China",
+    "se": "Sweden",
+    "hk": "Hong Kong",
+    "fr": "France"
+}
+const quocgia = {
+    "Vietnam": "Việt Nam",
+    "Canada": "Canada",
+    "US": "Hoa Kì",
+    "South Korea": "Hàn Quốc",
+    "Austria": "Úc",
+    "Mainland China": "Trung Quốc",
+    "Sweden": "Thuỵ Điển",
+    "Hong Kong": "Hong Kong",
+    "France": "Pháp"
+}
 module.exports = {
     name: "corona",
     category: "info",
     description: "Thông tin về coronavirus",
-    usage: " `_corona` hoặc `_corona <tên quốc gia>`",
-    note: "Quốc gia đang hỗ trợ: Việt Nam (vn), Hàn Quốc (kr), Hoa Kì (us), Canada (can)",
+    usage: " `_corona` hoặc `_corona <mã quốc gia 2 chữ>`",
+    note: "Quốc gia đang hỗ trợ: VN, CA, KR, AU, CN, SE, HK, FR ",
     run: async(client, message, args) => {
         if (!args[0]) {
             graphqlclient.request(query).then(data => {
@@ -51,112 +73,56 @@ module.exports = {
                     die = die + parseInt(count.Deaths);
                     recovered = recovered + parseInt(count.Recovered);
                 });
-                var confirmed = confirmed
-                    .toString()
-                    .replace(/(-?\d+)(\d{3})/g, "$1,$2"); //Thêm dấu phẩy sau 3 chữ số (75,748)
+                var confirmed = confirmed.toString().replace(/(-?\d+)(\d{3})/g, "$1,$2"); //Thêm dấu phẩy sau 3 chữ số (75,748)
                 var die = die.toString().replace(/(-?\d+)(\d{3})/g, "$1,$2");
-                var recovered = recovered
-                    .toString()
-                    .replace(/(-?\d+)(\d{3})/g, "$1,$2");
+                var recovered = recovered.toString().replace(/(-?\d+)(\d{3})/g, "$1,$2");
                 const embed = new RichEmbed()
                     .setAuthor(`Dữ liệu được tự động cập nhật`)
                     .setTitle(`Thông tin về virus Corona (nCoV, COVID-19)`)
                     .addField(`Số lượng ca nhiễm: `, `${confirmed} ca`)
                     .addField(`Số người chết: `, `${die} người`)
                     .addField(`Số người hội phục: `, `${recovered} người`)
-                    .setFooter(
-                        `Nguồn: corona.kompa.ai | Made by phamleduy04#9999\nThông tin cập nhật theo thời gian thực!`
-                    );
+                    .setFooter('Nguồn: corona.kompa.ai | Made by phamleduy04#9999');
                 message.channel.send(embed);
             });
-        } else if (args[0].toLowerCase() == "us") {
+        }
+        if (args.join(' ') == "vn full") {
             graphqlclient.request(query).then(res => {
-                var us = res.countries.filter(find => find.Country_Region == "US");
-                var us = us[0];
-                var us_updatetimestamp = new Date(parseInt(us.Last_Update))
-                var us_updatedate = us_updatetimestamp.getDate() + '/' + (us_updatetimestamp.getMonth()+1) + '/' + us_updatetimestamp.getFullYear()
-                const us_embed = new RichEmbed()
-                    .setAuthor(`Dữ liệu được tự động cập nhật`)
-                    .setTitle(`Số ca nhiễm COVID-19 ở Hoa Kỳ (US)`)
-                    .addField(`Số ca đẵ xác nhận: `, `${us.Confirmed} ca`)
-                    .addField(`Số ca tử vong: `, `${us.Deaths} ca`)
-                    .addField(`Số ca đã hồi phục: `, `${us.Recovered} ca`)
-                    .addField(`Cập nhật vào ngày: `, us_updatedate)
-                    .setFooter(`Nguồn: corona.kompa.ai | Made by phamleduy04#9999`);
-                message.channel.send(us_embed);
-            });
-        } else if (
-            args[0].toLowerCase() == "vn" || args[0].toLowerCase() == "vietnam"
-        ){
-            if (args[1] && args[1].toLowerCase() == 'full'){
-                graphqlclient.request(query).then(res => {
-                    const vn_full_embed = new RichEmbed()
-                        .setAuthor(`Dữ liệu được tự động cập nhật`)
-                        .setTitle(`Số ca nhiễm COVID-19 ở Việt Nam (chi tiết)`)
-                        .setFooter(`Nguồn: corona.kompa.ai | Made by phamleduy04#9999`)
-                    res.provinces.forEach(tentp => {
-                        vn_full_embed.addField(`${tentp.Province_Name} có: `,`${tentp.Confirmed} ca nhiễm, ${tentp.Deaths} ca tử vong và ${tentp.Recovered} ca hồi phục.`)
-                    })
-                    message.channel.send(vn_full_embed)
-                })
-            } else {
-                graphqlclient.request(query).then(res => {
-                    var vietnam = res.countries.filter(find => find.Country_Region == "Vietnam");
-                    var vietnam = vietnam[0]
-                    var vn_timestamp = new Date(parseInt(vietnam.Last_Update))
-                    var vn_updatedate = vn_timestamp.getDate() + '/' + (vn_timestamp.getMonth()+1) + '/' + vn_timestamp.getFullYear()
-                    const vn_embed = new RichEmbed()
-                        .setAuthor(`Dữ liệu được tự động cập nhật`)
-                        .setTitle(`Số ca nhiễm COVID-19 ở Việt Nam`)
-                        .setDescription(`Tips: Sử dụng lệnh \`_corona vn full\` để hiển thị chi tiết.`)
-                        .addField(`Số ca đẵ xác nhận: `, `${vietnam.Confirmed} ca`)
-                        .addField(`Số ca tử vong: `, `${vietnam.Deaths} ca`)
-                        .addField(`Số ca đã hồi phục: `, `${vietnam.Recovered} ca`)
-                        .addField(`Ngày cập nhật: `, vn_updatedate)
-                        .setFooter(`Nguồn: corona.kompa.ai | Made by phamleduy04#9999 `);
-                    message.channel.send(vn_embed);
+                var all_confirmed = 0;
+                var all_die = 0;
+                var all_recovered = 0;
+                let table = new ascii("Tình hình COVID-19 ở Việt Nam")
+                table.setHeading("Tỉnh thành", "Phát hiện", "Tử vong", "Bình phục")
+                res.provinces.forEach(tentp => {
+                    all_confirmed = all_confirmed + parseInt(tentp.Confirmed)
+                    all_die = all_die + parseInt(tentp.Deaths)
+                    all_recovered = all_recovered + parseInt(tentp.Recovered)
+                    table.addRow(tentp.Province_Name, tentp.Confirmed, tentp.Deaths, tentp.Recovered)
                 });
-            }
-        } else if (
-            args[0].toLowerCase() == "canada" || args[0].toLowerCase() == "can"
-        ) {
-            graphqlclient.request(query).then(res => {
-                var canada = res.countries.filter(
-                    find => find.Country_Region == "Canada"
-                );
-                var canada = canada[0];
-                var ca_timestamp = new Date(parseInt(canada.Last_Update))
-                var ca_updatedate = ca_timestamp.getDate() + '/' + (ca_timestamp.getMonth()+1) + '/' + ca_timestamp.getFullYear()
-                const can_embed = new RichEmbed()
-                    .setAuthor(`Dữ liệu được tự động cập nhật`)
-                    .setTitle(`Số ca nhiễm COVID-19 ở Canada`)
-                    .addField(`Số ca đẵ xác nhận: `, `${canada.Confirmed} ca`)
-                    .addField(`Số ca tử vong: `, `${canada.Deaths} ca`)
-                    .addField(`Số ca đã hồi phục: `, `${canada.Recovered} ca`)
-                    .addField(`Ngày cập nhật: `,ca_updatedate)
-                    .setFooter(`Nguồn: corona.kompa.ai | Made by phamleduy04#9999 `);
-                message.channel.send(can_embed);
+                table.addRow("Tổng cộng", all_confirmed.toString(), all_die.toString(), all_recovered.toString())
+                return message.channel.send(table.toString(), {
+                    code: 'md'
+                });
             });
-        } else if (korean_list.indexOf(args[0].toLowerCase()) > -1) {
-            graphqlclient.request(query).then(res => {
-                var korean = res.countries.filter(
-                    find => find.Country_Region == "South Korea"
-                );
-                var korean = korean[0];
-                var kr_timestamp = new Date(parseInt(korean.Last_Update))
-                var kr_updatedate = kr_timestamp.getDate() + '/' + (kr_timestamp.getMonth()+1) + '/' + kr_timestamp.getFullYear()
-                const kr_embed = new RichEmbed()
+        } else if (search[args[0]]) {
+            graphqlclient.request(query).then(result => {
+                var json_data = result.countries.filter(find => find.Country_Region == search[args[0]])
+                var json_data = json_data[0];
+                var timestamp = new Date(parseInt(json_data.Last_Update))
+                var date = timestamp.getDate() + '/' + (timestamp.getMonth() + 1) + '/' + timestamp.getFullYear()
+                const embed = new RichEmbed()
                     .setAuthor(`Dữ liệu được tự động cập nhật`)
-                    .setTitle(`Số ca nhiễm COVID-19 ở Hàn Quốc`)
-                    .addField(`Số ca đẵ xác nhận: `, `${korean.Confirmed} ca`)
-                    .addField(`Số ca tử vong: `, `${korean.Deaths} ca`)
-                    .addField(`Số ca đã hồi phục: `, `${korean.Recovered} ca`)
-                    .addField(`Ngày cập nhật: ` ,kr_updatedate)
+                    .setTitle(`Số ca nhiễm COVID-19 ở ${quocgia[search[args[0]]]} `)
+                    .addField(`Số ca đẵ xác nhận: `, `${json_data.Confirmed} ca`)
+                    .addField(`Số ca tử vong: `, `${json_data.Deaths} ca`)
+                    .addField(`Số ca đã hồi phục: `, `${json_data.Recovered} ca`)
+                    .addField(`Cập nhật vào ngày: `, date)
                     .setFooter(`Nguồn: corona.kompa.ai | Made by phamleduy04#9999`);
-                message.channel.send(kr_embed);
+                if (search[args[0]] == "Vietnam") embed.setDescription(`Tips: Sử dụng lệnh \`_corona vn full\` để hiển thị chi tiết.`)
+                message.channel.send(embed)
             });
         } else {
-            message.channel.send(`Đất nước bạn đang tìm kiếm bot chưa hỗ trợ, hãy quay lại sau nhé!`);
+            message.channel.send(`Đất nước bạn đang tìm kiếm bot chưa hỗ trợ, hãy quay lại sau nhé!`)
         }
     }
-};
+}
