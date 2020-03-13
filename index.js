a4141621b6f227edfd15c3082bbbec52277ca039
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-03-13 18:22:44 -0500

    agentbot (v4.1.0) release

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ client.on("ready", () => {
 
     client.user.setPresence({
         status: "online",
-        game: {
+        activity: {
             name: "me getting developed",
             type: "PLAYING"
         }
