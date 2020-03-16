173a583d831d5576c2e84b495390af55b70d1efd
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-03-16 15:24:58 -0400

    fix bugs

--- commands/moderation/leave.js
+++ commands/moderation/leave.js
@@ -4,8 +4,9 @@ module.exports = {
     description: "Leave sv",
     usage: "_leave",
     run: (client, message, args) => {
-        if (message.author !== "621890949244518451")
+        if (message.author == "621890949244518451"){
             message.guild.leave()
             .then(g => console.log(`Left the guild ${g}`))
+        }
     }
 }
