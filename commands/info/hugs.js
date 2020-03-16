70e4e197fca77d0126c05f85afca62b1631aa418
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-03-16 15:24:20 -0400

    Delete hugs.js

--- commands/info/hugs.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-module.exports = {
-    name: "hugs",
-    aliases: [""],
-    category: "",
-    description: "",
-    usage: "",
-    run: async(client, message, args) => {}
-}
