cf192176a7149078930faef1c42e79e77428b791
Duy Pham <phamleduy04@gmail.com>
2020-03-17 22:45:12 -0400

    xoa het command cu

--- README.md
+++
@@ -1,8 +0,0 @@
-# agentbot
-## To install all packages use:
-
-```npm i```
-
-## To run the bot please use:
-
-```nodemon -e js,json,txt```
--- commands/info/ancom.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "ancom",
-    aliases: ["dinner"],
-    run: (client, message, args) => {
-        message.delete();
-        message.channel.send("Địt mẹ tao đang ăn cơm hú cc");
-    }
-}
